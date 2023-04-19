bedaff431085443ac5e473845f2444c7bbeffbd1
Szelei Krisztian <kszelei99@gmail.com>
2023-04-19 12:43:42 +0200

    Created question page for admin

--- src/app/services/api-interceptor.service.ts
+++ src/app/services/api-interceptor.service.ts
@@ -1,16 +1,18 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { error } from 'console';
+import { Observable, catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if(localStorage.getItem('token') == null) return next.handle(request);
+    if(localStorage.getItem('token') === null) return next.handle(request);
 
     request = request.clone({
       setHeaders: {
@@ -18,6 +20,13 @@ export class TokenInterceptor implements HttpInterceptor {
       }
     });
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if(error instanceof HttpErrorResponse && error.status === 403) {
+          return this.authService.logout();
+        }
+        return throwError(() => error);
+      })
+    )
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,11 +5,15 @@ import { RegisterRequest } from '../model/auth/register-request';
 import { environment } from 'src/environments/environment';
 import { Observable, of, tap } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   login(request: AuthenticationRequest): Observable<any> {
     return this.http.post<any>(environment.backendUrl + '/auth/authenticate', request)
@@ -20,6 +24,8 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem("token");
+    this.router.navigate(['/login']);
+    return this.http.get<any>(environment.backendUrl + '/auth/logout');
   }
 
   register(request: RegisterRequest): Observable<any> {
