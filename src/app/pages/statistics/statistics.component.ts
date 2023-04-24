0433a865a78465e348f2e25019e7496fe269c16e
adammakai8 <adammakai8@gmail.com>
2023-04-24 17:04:42 +0200

    Displaying statistics

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { StatisticsGroup } from 'src/app/model/statistics/statisticsgroup';
 import { ThemeCount } from 'src/app/model/statistics/themecount';
 import { StatisticsService } from 'src/app/services/statistics.service';
 
@@ -18,6 +19,8 @@ export class StatisticsComponent implements OnInit {
   activeTab: Tab = Tab.AGE;
 
   popularThemes: ThemeCount[] = [];
+  statisticsByAge: StatisticsGroup[] = [];
+  statisticsByTheme: StatisticsGroup[] = [];
 
   constructor(
     private translate: TranslateService,
@@ -27,6 +30,8 @@ export class StatisticsComponent implements OnInit {
   ngOnInit(): void {
     this.statisticsService.getGlobalStatistics().subscribe(stats => {
       this.popularThemes = stats.popularThemes.sort((a, b) => b.gameCount - a.gameCount);
+      this.statisticsByAge = stats.statisticsByAge;
+      this.statisticsByTheme = stats.statisticsByTheme;
     });
   }
 
